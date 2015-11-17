280af720cc8326ff9145c1d7879a34b79ef8f2e5
Paul Horton <pahorton@asu.edu>
2015-11-17 00:07:49 -0700

    Color and status
    Updated the colors and made the status show score and give player
    prompts

--- TheGameGooey.java
+++ TheGameGooey.java
@@ -7,10 +7,11 @@ public class TheGameGooey{
 	private JFrame window; 
 	private JPanel gameGrid; //Grid that holds frames
 	private JPanel frameGrid; //Frames
-	private JLabel statusBar, turnIndicator; //Text indicators
+	private JLabel statusBar, scoreIndicator; //Text indicators
 	private boolean mode; //true = full view, false = subView
 	private final int GAME_DIM = 600; //Game dimensions
 	private boolean gameWon;
+	private Scheme base;
 
 	//Buttons that hold information on which field and tile it is. 
 	private class EButton extends JButton{
@@ -28,13 +29,9 @@ public class TheGameGooey{
 	//Swaps the turn of the player and changes the turnIndocator to indicate who is playing.
 	private void changeTurn() {
 		theGame.changeActivePlayer();
-
-		if (theGame.getActivePlayer()) {
-			turnIndicator.setText("PLAYER 2");
-		}
-		else {
-			turnIndicator.setText("PLAYER 1");
-		}
+		scoreIndicator.setText(base.player1name + ": " + theGame.getP1Score() + 
+			"       " + base.player2name + ": " + theGame.getP2Score() +
+			"       Fields left in play: " + theGame.getWinnableFields());
 	}
 
 	//Changes the text in the status bar.
@@ -50,11 +47,11 @@ public class TheGameGooey{
 			Field clickedField = theGame.getField(butt.parentField); //Gets the field in question
 			Tile clickedTile = theGame.getTile(butt.parentField,butt.content); //Gets the tile in question
 
-			setStatus("Please play in the green field"); //Resets the Status text
+			setStatus("Please play in the green field, " + (theGame.getActivePlayer()? base.player2name : base.player1name)); //Set owner); //Resets the Status text
 			if(gameWon) {
 				theGame.reset();
 				gameWon = false;
-				setStatus("Welcome to #TheGame");
+				setStatus("Welcome to #TheGame! Press any square to start, " + base.player1name);
 				recolor();
 				return;
 			}
@@ -65,17 +62,17 @@ public class TheGameGooey{
 					if (clickedField.checkIfWon()) { //Check if field is won
 						theGame.incScore(theGame.getActivePlayer()); //Increases the score of the player who won
 						if(theGame.checkIfWon()){ //Check if game is won if field is won
-							setStatus("Player " + (theGame.getActivePlayer()? 2 : 1) + " won! Press any button to play again");
+							setStatus((theGame.getActivePlayer()? base.player2name : base.player1name) + " won! Press any button to play again");
 							gameWon = true;
 							recolor();
 							return;	
 						} 
-						setStatus("Player " + (theGame.getActivePlayer()? 2 : 1) + " won a field.");
+						setStatus((theGame.getActivePlayer()? base.player2name : base.player1name) + " won a field.");
 					}
 					else if (clickedField.isFull() && clickedField.getOwner() == 0) { //Check if field was catsgamed
 						theGame.decWinnableFields(); //Decreases winnable fields due to cats game
 						if(theGame.checkIfWon()){ //Check if game is won if field is won
-							setStatus("Player " + (theGame.getActivePlayer()? 2 : 1) + " won! Press any button to play again");
+							setStatus((theGame.getActivePlayer()? base.player2name : base.player1name) + " won! Press any button to play again");
 							gameWon = true;
 							recolor();
 							return;
@@ -87,12 +84,12 @@ public class TheGameGooey{
 						theGame.setFieldInPlay(butt.content);                            
 					}
 					else { //The field the next player was sent is full
-						setStatus("You can play anywhere, Player " + (theGame.getActivePlayer()? 2 : 1));
+						setStatus("You can play anywhere, " + (theGame.getActivePlayer()? base.player2name : base.player1name));
 						theGame.setFieldInPlay(-1);
 					}
 				}
 				else { //Clicked Tile is not free
-					setStatus("That's not a free tile, Player " + (theGame.getActivePlayer()? 2 : 1));
+					setStatus("That's not a free tile, " + (theGame.getActivePlayer()? base.player2name : base.player1name));
 				}
 			}
 
@@ -103,17 +100,17 @@ public class TheGameGooey{
 					if (clickedField.checkIfWon()) { //Check if field is won
 						theGame.incScore(theGame.getActivePlayer()); //Increases the score of the player who won
 						if(theGame.checkIfWon()){ //Check if game is won if field is won
-							setStatus("Player " + (theGame.getActivePlayer()? 2 : 1) + " won! Press any button to play again");
+							setStatus((theGame.getActivePlayer()? base.player2name : base.player1name) + " won! Press any button to play again");
 							gameWon = true;
 							recolor();
 							return;	
 						} 
-						setStatus("Player " + (theGame.getActivePlayer()? 2 : 1) + " won a field.");
+						setStatus((theGame.getActivePlayer()? base.player2name : base.player1name) + " won a field.");
 					}
 					else if (clickedField.isFull() && clickedField.getOwner() == 0) { //Check if field was catsgamed
 						theGame.decWinnableFields(); //Decreases winnable fields due to cats game
 						if(theGame.checkIfWon()){ //Check if game is won if field is won
-							setStatus("Player " + (theGame.getActivePlayer()? 2 : 1) + " won! Press any button to play again");
+							setStatus((theGame.getActivePlayer()? base.player2name : base.player1name) + " won! Press any button to play again");
 							gameWon = true;
 							recolor();
 							return;
@@ -121,21 +118,20 @@ public class TheGameGooey{
 					}
 					theGame.setLastIndex(butt.index());
 					changeTurn();
-
 					if (!(theGame.getField(butt.content).isFull())) { //Set the next field to play in to the previous player's tile
 						theGame.setFieldInPlay(butt.content);                            
 					}
 					else { //The field the next player was sent is full
-						setStatus("You can play anywhere, Player " + (theGame.getActivePlayer()? 2 : 1));
+						setStatus("You can play anywhere, " + (theGame.getActivePlayer()? base.player2name : base.player1name));
 						theGame.setFieldInPlay(-1);
 					}
 				}
 				else { //Clicked Tile is not free
-					setStatus("That's not a free tile, Player " + (theGame.getActivePlayer()? 2 : 1));
+					setStatus("That's not a free tile, " + (theGame.getActivePlayer()? base.player2name : base.player1name));
 				}
 			}
 			else {
-				setStatus("That's not the right field, Player " + (theGame.getActivePlayer()? 2 : 1));
+				setStatus("That's not the right field, Player " + (theGame.getActivePlayer()? base.player2name : base.player1name));
 			}
 			recolor();
 		}
@@ -155,12 +151,14 @@ public class TheGameGooey{
 		window = new JFrame();
 
 		fullView();
-		statusBar = new JLabel("Welcome to #TheGame");
-		turnIndicator = new JLabel("PLAYER 1");
+		statusBar = new JLabel("Welcome to #TheGame! Press any square to start, " + base.player1name);
+		scoreIndicator = new JLabel(base.player1name + ": " + theGame.getP1Score() + 
+			"       " + base.player2name + ": " + theGame.getP2Score() +
+			"       Fields left in play: " + theGame.getWinnableFields());
 
 		window.add(statusBar);
 		window.add(gameGrid);
-		window.add(turnIndicator);
+		window.add(scoreIndicator);
 
 		window.setLayout(new BoxLayout(window.getContentPane(),BoxLayout.Y_AXIS));
 		window.pack();
@@ -185,11 +183,10 @@ public class TheGameGooey{
 		mode = false;
 	}
 	private void recolor(){//recolors all buttons. will recolor for either view
-		Scheme base = new Scheme();
 		for(EButton e: buttons){
 			e.setOpaque(true);
 			//Color all blank
-			e.setBackground(base.blank);
+			e.setBackground(base.tile);
 			//Color the field in play
 			if(e.parentField == theGame.getFieldInPlay() || theGame.getFieldInPlay() == -1) {
 				e.setBackground(base.field);
@@ -203,12 +200,16 @@ public class TheGameGooey{
 				e.setBackground(theGame.getActivePlayer()? base.player1new : base.player2new);
 			}
 		}
-
+		gameGrid.setBackground(base.board);
+		for(Component c : gameGrid.getComponents()) {
+			c.setBackground(base.board);
+		}
 	}
 	public void endView() {} //End
 	public TheGameGooey(){
 		theGame = new Board();
 		gameWon = false;
+		base = new Scheme();
 		initFrame();
 	}
 	public static void main(String[] args){
