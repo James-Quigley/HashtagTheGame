20fc11567f19e29c98c6581d4184f585db1e5f4f
dkoleber <dkedusp@gmail.com>
2015-11-23 17:51:38 -0700

    small changes. added buttonlistener class

---
+++ HashtagTheGameAndroid/HashtagTheGame/src/main/java/ser215/hashtagthegameandroid/ButtonListener.java
@@ -0,0 +1,18 @@
+package ser215.hashtagthegameandroid;
+import android.content.*;
+import android.view.*;
+import android.app.*;
+/**
+ * Created by Kirevikyn on 11/23/2015.
+ */
+public class ButtonListener implements View.OnClickListener {
+    public void onClick(View v){
+        //do stuff lel
+        int id = v.getId();
+        //do some maths to determine which button was pressed
+        // go fokin 'am m8
+
+
+
+    }
+}
