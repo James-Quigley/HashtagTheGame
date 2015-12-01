c5a42d14e0cf467576d229fd9f79b9f08cbe433d
James-Quigley <james@quigley.us>
2015-11-30 22:14:51 -0700

    Rewrote class to be functional

--- HashtagTheGameAndroid/HashtagTheGame/src/main/java/ser215/hashtagthegameandroid/AndroidGooey.java
+++ HashtagTheGameAndroid/HashtagTheGame/src/main/java/ser215/hashtagthegameandroid/AndroidGooey.java
@@ -1,127 +1,84 @@
 package ser215.hashtagthegameandroid;
 
-public class AndroidGooey{
-    private EButton[] buttons;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AndroidGooey extends AppCompatActivity {
+    private Button[] buttons = new Button[81];
     private Board theGame;
     //private JFrame window;
     //private JPanel grid;
     //private JLabel statusBar, turnIndicator;
     private final int GAME_DIM = 600;
-    private class EButton{
-        private int parentField; //index of parent field in fullviewlayout (0-8)
-        private int content; //index of tile in its parent frame (0-8)
-        public EButton(int f, int t){
-            parentField = f;
-            content = t;
-        }
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fullviewlayout);
+        theGame = new Board();
+        for (int i = 0; i < 81; i++) {
+            String buttonID = "button"+(i+1);
+            int id = getResources().getIdentifier(buttonID, "id", getPackageName());
+            buttons[i] = ((Button) findViewById(id));
+            buttons[i].setText(i + "");
+            buttons[i].setTextColor(Color.TRANSPARENT);
+        }
+        recolor();
     }
 
+    public void createToast(String message){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
 
+        Toast toast = Toast.makeText(context, message, duration);
+        toast.show();
+    }
 
-    private class ButtonListener{
-        public void actionPerformed() {
-            EButton butt = new EButton(0, 0);//(EButton)e.getSource(); //Loads the button information with data values
-            Field clickedField = theGame.getField(butt.parentField); //Gets the field in question
-            Tile clickedTile = theGame.getTile(butt.parentField, butt.content); //Gets the tile in question
+    public void buttonClicked(View view){
+        Button button = (Button)view;
 
-            boolean gameWon = theGame.checkIfWon();
-            int fip = theGame.getFieldInPlay();
-            //end conditions
-            if (fip == -1) {//if in full view
-                if (!clickedField.isFull()) {
-                    subView(butt.parentField);
-                } else {
-                }//alert user that the field is full
-            } else {//already in a subview
-                int move = theGame.makePlayAt(butt.parentField, butt.content);
+        int buttonID = Integer.parseInt(button.getText().toString());
+        //Field clickedField = theGame.getField((int)buttonID/9); //Gets the field in question
+        //Tile clickedTile = theGame.getTile((int)buttonID/9, buttonID%9); //Gets the tile in question
+        //boolean gameWon = theGame.checkIfWon();
+        //int fip = theGame.getFieldInPlay();
+        //end conditions
+        int move = theGame.makePlayAt((int)buttonID/9, buttonID%9);
+        if (move == 0) {//invalid move
+            createToast("Invalid Move");
+        }
+        if (move == 1) {//valid move made, new field in play
+        }
+        if (move == 2) {//valid move, choose field in play
+        }
+        if (move == 3) {//game over
+            endView();
+        }
 
-                if (move == 0) {//invalid move
-                }
-                if (move == 1) {//valid move made, new field in play
-                }
-                if (move == 2) {//valid move, choose field in play
-                }
-                if (move == 3) {//game over
-                    endView();
-                }
+        recolor();
+    }
+
+    protected void recolor(){//recolors all buttons. will recolor for either view
+        for(int i = 0; i < 81; i++){
+            int t = theGame.getField((int)i/9).getTile(i%9).getOwner();
+            if(t == 0) buttons[i].setBackgroundColor(0xFFFFFFFF); // 0xAARRGGBB
+            if(t == 1) buttons[i].setBackgroundColor(0xFFC34C1D);
+            if(t == 2) buttons[i].setBackgroundColor(0xFF1F42F0);
+            if(theGame.getLastIndex() == i) {
+                buttons[i].setBackgroundColor(0xFF98291E);
             }
         }
-    }
-    public void initButtons(){
-        buttons = new EButton[81];
-        for(int i = 0;i<81;i++){
-            buttons[i] = new EButton(i/9,i%9);
-            //buttons[i].addActionListener(new ButtonListener());
-        }
 
     }
-    public void initFrame(){
-        initButtons();
-        //window = new JFrame();
-
-
-        //window.setLayout(new FlowLayout());
-        fullView();
-        //statusBar = new JLabel("EMPTY");
-        //turnIndicator = new JLabel("PLAYER 1");
-
-        //window.add(grid);
-        //window.add(statusBar);
-        //window.add(turnIndicator);
-		/*
-		initiate frame
-		add buttons
-		add status bar
-		add turn indicator
-		*/
-        //window.pack();
-        //window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        //window.setVisible(true);
+    public void endView() {
+        theGame.reset();
     }
-    public void fullView(){//sets grid to 9x9 view
-        //grid = new JPanel(new GridLayout(9,9));
 
-        for(int i = 0;i<3;i++)
-            for(int j = 0;j<3;j++)
-                for(int k = 0;k<3;k++)
-                    k = k;
-                    //grid.add(buttons[(i*3)+(j*9)+k]);
-        for(int i = 9;i<12;i++)
-            for(int j = 0;j<3;j++)
-                for(int k = 0;k<3;k++)
-                    k=k;
-                    //grid.add(buttons[(i*3)+(j*9)+k]);
-        for(int i = 18;i<21;i++)
-            for(int j = 0;j<3;j++)
-                for(int k = 0;k<3;k++)
-                    k=k;
-                    //grid.add(buttons[(i*3)+(j*9)+k]);
-        //for(int i = 0;i<81;i++) grid.add(buttons[i]);
-        recolor();
-        //grid.setPreferredSize(new Dimension(GAME_DIM, GAME_DIM));
-
-    }
-    public void subView(int f){
-    }
-    private void recolor(){//recolors all buttons. will recolor for either view
-        for(EButton e: buttons){
-            int t = theGame.getField(e.parentField).getTile(e.content).getOwner();
-            //e.setOpaque(true);
-            //if(t == 0) e.setBackground(Color.WHITE);
-            //if(t == 1) e.setBackground(Color.ORANGE);
-            //if(t == 2) e.setBackground(Color.BLUE);
-            if(theGame.getLastIndex() == e.parentField*9+e.content){}
-                //e.setBackground(Color.RED);
-        }
-
-    }
-    public void endView() {} //end of game
-    public AndroidGooey(){
-        theGame = new Board();
-        initFrame();
-    }
-    public static void main(String[] args){
-        AndroidGooey g = new AndroidGooey();
-    }
 }
