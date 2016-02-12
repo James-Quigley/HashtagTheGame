3124b928dfb0e85b49ebf79120f8cc40efe9f16e
James Quigley <james@quigley.us>
2016-02-12 12:28:57 -0700

    Created GameBoard and added buttons

---
+++ HashtagTheGame/app/src/main/java/poly_cs_club/hashtagthegame/GameBoard.java
@@ -0,0 +1,13 @@
+package poly_cs_club.hashtagthegame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameBoard extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_board);
+    }
+}
