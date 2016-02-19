230ada679f1e87b74c194d5c17938b19584c794d
James Quigley <james@quigley.us>
2016-02-19 12:13:43 -0700

    Added intents to play buttons to start new game

--- HashtagTheGame/app/src/main/java/poly_cs_club/hashtagthegame/MainMenu.java
+++ HashtagTheGame/app/src/main/java/poly_cs_club/hashtagthegame/MainMenu.java
@@ -1,7 +1,9 @@
 package poly_cs_club.hashtagthegame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends Activity {
 
@@ -9,5 +11,20 @@ public class MainMenu extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void onClick(View view){
+        Intent myIntent = new Intent(this, GameBoard.class);
+        if(view.getId() == R.id.singlePlayer) {
+            myIntent.putExtra("gameType", "single");
+        }
+        else if (view.getId() == R.id.local){
+            myIntent.putExtra("gameType", "local");
+        }
+        else if (view.getId() == R.id.online){
+            myIntent.putExtra("gameType", "online");
+        }
+        this.startActivity(myIntent);
+
     }
 }
